3c2dc02138681c9650adac8686b5f512272f869b
Dawid <dawidsobieszczuk@gmail.com>
2024-02-03 13:01:19 +0100

    Add GoogleMaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GoogleMap } from "@angular/google-maps"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GoogleMap],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   
+  display: any;
+  center: google.maps.LatLngLiteral = {
+      lat: 52,
+      lng: 19
+  };
+  zoom = 6;
+  moveMap(event: google.maps.MapMouseEvent) {
+      if (event.latLng != null) this.center = (event.latLng.toJSON());
+  }
+  move(event: google.maps.MapMouseEvent) {
+      if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
 }
